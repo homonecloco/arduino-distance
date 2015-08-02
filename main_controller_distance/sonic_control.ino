2179b3ebe18d380f9bd2f5251eaadfc416a1f4c7
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-08-02 19:13:56 +0100

    Added sort to the array to drop one decile on each side of the messurements to calculate the average

--- main_controller_distance/sonic_control.ino
+++ main_controller_distance/sonic_control.ino
@@ -91,9 +91,42 @@ bool sonic_sensor_is_buffer_overflow(){
 
 float sonic_sensor_average(){
   //return (float) SONIC_BUFFER_AVG;
-  return ( (float)SONIC_SENSOR_OFFSET ) - ((float)SONIC_BUFFER_AVG/(float)SONIC_BUFFER_USED)  / STORE_PRECISION;
+  quicksort(SONIC_BUFFER, 0, SONIC_BUFFER_USED - 1);
+  int drop = SONIC_BUFFER_USED * 0.1;
+  int i;
+  int sum = 0;
+  int count = 0;
+  for(i = drop; i < SONIC_BUFFER_USED - drop; i++){
+    sum += SONIC_BUFFER[i];
+    count ++;
+  }
+
+  return ( (float)SONIC_SENSOR_OFFSET ) - ((float)sum/(float)count)  / STORE_PRECISION;
 }
 
 void sonic_sensor_set_offset(int offset){
   SONIC_SENSOR_OFFSET = offset;
+}
+
+/* replaced by luke through quicksort -> seems to be the fastest to process information on Arduino ??? */
+////////////////////////////////////////
+void quicksort(int a[], int l, int r){            //a=Array, l=left border, r=right border  
+  if(r>l){                              //as long that more then 1 followingelement exists do
+    int i=l-1, j=r;                  //Initialising the variables with the borders
+    int tmp;
+    for(;;){                        //endless loop; stops if i>=j
+      while(a[++i]<a[r]);                  //inkrem., until found bigger element
+      while(a[--j]>a[r] && j>i);            //dekrem., until found smaler element
+      if(i>=j) break;                   //stops run if pointer meet
+      tmp=a[i];                        //exchange smaller with bigger element
+      a[i]=a[j];                       //exchange smaller with bigger element
+      a[j]=tmp;                        //exchange smaller with bigger element
+    }
+    tmp=a[i];           //exchange dividing element
+    a[i]=a[r];          //exchange dividing element
+    a[r]=tmp;            //exchange dividing element
+
+    quicksort(a, l, i-1);                  //recursive call of function for left
+    quicksort(a, i+1, r);                  //recursive call of function for right
+  }
 }
